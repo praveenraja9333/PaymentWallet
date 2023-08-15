69eb58d25c1f48cc9a474f7de8b93e8dc2aac856
Praveen Rajendran <praveenraja9333@gmail.com>
2023-08-15 12:54:01 +0100

    Initial Commit

---
+++ src/test/java/com/vrp/system/paymentsystem/paymentwallet/PaymentwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrp.system.paymentsystem.paymentwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
